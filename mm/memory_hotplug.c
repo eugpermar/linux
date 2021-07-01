b1329e97b00fc59d800ca03bd896f3c320948f84
Eugenio Pérez <eperezma@redhat.com>
2021-07-01 18:58:16 +0200

    Revert "mm/memory_hotplug: update comment regarding zone shuffling"
    This reverts commit b86c5fc4e71a23e284b7cdfb757fb76534ab4119.

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -967,10 +967,13 @@ int __ref online_pages(unsigned long pfn, unsigned long nr_pages, struct zone *z
 	undo_isolate_page_range(pfn, pfn + nr_pages, MIGRATE_MOVABLE);
 
 	/*
-	 * Freshly onlined pages aren't shuffled (e.g., all pages are placed to
-	 * the tail of the freelist when undoing isolation). Shuffle the whole
-	 * zone to make sure the just onlined pages are properly distributed
-	 * across the whole freelist - to create an initial shuffle.
+	 * When exposing larger, physically contiguous memory areas to the
+	 * buddy, shuffling in the buddy (when freeing onlined pages, putting
+	 * them either to the head or the tail of the freelist) is only helpful
+	 * for maintaining the shuffle, but not for creating the initial
+	 * shuffle. Shuffle the whole zone to make sure the just onlined pages
+	 * are properly distributed across the whole freelist. Make sure to
+	 * shuffle once pageblocks are no longer isolated.
 	 */
 	shuffle_zone(zone);
 
